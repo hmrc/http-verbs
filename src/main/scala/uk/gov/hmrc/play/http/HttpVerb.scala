48ac92e854637e82e05758a231c4a0e39d69f233
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-03-13 09:49:48 +0000

    Tidy up HttpErrorFunctions & add unit spec

--- src/main/scala/uk/gov/hmrc/play/http/HttpVerb.scala
+++ src/main/scala/uk/gov/hmrc/play/http/HttpVerb.scala
@@ -16,20 +16,10 @@
 
 package uk.gov.hmrc.play.http
 
-import java.net.ConnectException
-import java.util.concurrent.TimeoutException
-import uk.gov.hmrc.play.audit.http.HeaderCarrier
-
-import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
 trait HttpVerb extends HttpErrorFunctions {
 
   type ProcessingFunction = (Future[HttpResponse], String) => Future[HttpResponse]
 
-  protected[http] def mapErrors(httpMethod: String, url: String, f: Future[HttpResponse]): Future[HttpResponse] =
-    f.recover {
-      case e: TimeoutException => throw new GatewayTimeoutException(gatewayTimeoutMessage(httpMethod, url, e))
-      case e: ConnectException => throw new BadGatewayException(badGatewayMessage(httpMethod, url, e))
-  }
 }
---
+++ src/test/scala/uk/gov/hmrc/play/http/HttpErrorFunctionsSpec.scala
@@ -0,0 +1,42 @@
+package uk.gov.hmrc.play.http
+
+import org.scalacheck.Gen
+import org.scalatest.matchers.{MatchResult, Matcher, HavePropertyMatchResult, HavePropertyMatcher}
+import org.scalatest.prop.{TableDrivenPropertyChecks, GeneratorDrivenPropertyChecks}
+import org.scalatest.{TryValues, Matchers, WordSpec}
+
+import scala.util.{Failure, Try}
+
+class HttpErrorFunctionsSpec extends WordSpec with Matchers with GeneratorDrivenPropertyChecks with TableDrivenPropertyChecks with TryValues {
+
+  "HttpErrorFunctions" should {
+    "return the response if the status code is between 200 and 299" in new HttpErrorFunctions {
+      forAll (Gen.choose(200, 299)) { statusCode: Int =>
+        val expectedResponse = HttpResponse(statusCode)
+        handleResponse(exampleVerb, exampleUrl)(expectedResponse) should be (expectedResponse)
+      }
+    }
+    "return the correct exception if the status code is 400" in { expectA[BadRequestException](forStatus = 400) }
+    "return the correct exception if the status code is 404" in { expectA[NotFoundException]  (forStatus = 404) }
+    "return the correct exception for all other status codes" in {
+      forAll (Gen.choose(0, 199))                                        (expectA[Exception](_))
+      forAll (Gen.choose(400, 499).suchThat(!Seq(400, 404).contains(_))) (expectA[Upstream4xxResponse](_, Some(500)))
+      forAll (Gen.choose(500, 599))                                      (expectA[Upstream5xxResponse](_, Some(502)))
+      forAll (Gen.choose(600, 1000))                                     (expectA[Exception](_))
+    }
+  }
+
+  val exampleVerb = "GET"
+  val exampleUrl = "http://example.com/something"
+  val exampleBody = "this is the string body"
+
+  def expectA[T: Manifest](forStatus: Int, reportStatus: Option[Int] = None): Unit = new HttpErrorFunctions {
+    val e = Try(handleResponse(exampleVerb, exampleUrl)(HttpResponse(forStatus, responseString = Some(exampleBody)))).failure.exception
+    e should be (a [T])
+    e.getMessage should (include (exampleUrl) and include (exampleVerb) and include (exampleBody))
+    reportStatus.map { s =>
+      e should have ('upstreamResponseCode (forStatus))
+      e should have ('reportAs (s))
+    }
+  }
+}
