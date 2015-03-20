7157ff48ad3710701773245c381e6a895b537695
Howy Perrin <howard.perrin@digital.hmrc.gov.uk>
2015-03-20 19:57:01 +0000

    Rework README. Add Examples.scala

---
+++ src/test/scala/uk/gov/hmrc/play/Examples.scala
@@ -0,0 +1,38 @@
+package uk.gov.hmrc.play
+
+
+
+object Examples {
+
+  import uk.gov.hmrc.play.http._
+  import ws._
+  import audit.http.config._
+  import audit.http.connector._
+
+  trait ConnectorWithHttpValues {
+    val http: HttpGet with HttpPost
+  }
+  object ConnectorWithHttpValues extends ConnectorWithHttpValues {
+    val http = new WSGet with WSPost {
+      val appName = "my-app-name"
+      val auditConnector = AuditConnector(LoadAuditingConfig(key = "auditing"))
+    }
+  }
+
+  trait ConnectorWithMixins extends HttpGet with HttpPost
+  object ConnectorWithMixins extends ConnectorWithMixins with WSGet with WSPost {
+    val appName = "my-app-name"
+    val auditConnector = AuditConnector(LoadAuditingConfig(key = "auditing"))
+  }
+
+  trait VerbExamples {
+    val http: HttpGet with HttpPost with HttpPut with HttpDelete
+
+    http.GET("http://gov.uk/hmrc")
+    http.DELETE("http://gov.uk/hmrc")
+    http.POST("http://gov.uk/hmrc", body = "hi there")
+    http.PUT("http://gov.uk/hmrc", body = "hi there")
+
+
+  }
+}
