e6763a2ce26d9362eafb1136f978fd70ee533efd
Konrad Wudkowski <wudkowski@gmail.com>
2018-06-28 14:49:09 +0100

    PLATOPS-1387: Remove dependency on uk.gov.hmrc.time and logback

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -19,21 +19,19 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "com.typesafe.play" %% "play-json"      % "2.5.16",
-    "uk.gov.hmrc"       %% "time"           % "2.0.0",
-    "uk.gov.hmrc"       %% "http-core"      % "0.9.0",
-    "ch.qos.logback"    % "logback-core"    % "1.1.7",
-    "ch.qos.logback"    % "logback-classic" % "1.1.7"
+    "com.typesafe.play" %% "play-json" % "2.5.16",
+    "uk.gov.hmrc"       %% "http-core" % "0.9.0",
+    "org.slf4j"         % "slf4j-api"  % "1.7.25"
   )
 
   val test = Seq(
-    "commons-codec"          % "commons-codec" % "1.7"     % Test,
-    "org.scalatest"          %% "scalatest"    % "3.0.3"   % Test,
-    "org.scalacheck"         %% "scalacheck"   % "1.13.4"  % Test,
-    "org.pegdown"            % "pegdown"       % "1.6.0"   % Test,
-    "com.github.tomakehurst" % "wiremock"      % "1.52"    % Test,
-    "ch.qos.logback"         % "logback-core"  % "1.1.7"   % Test,
-    "org.mockito"            % "mockito-all"   % "1.10.19" % Test
+    "commons-codec"          % "commons-codec"   % "1.7"     % Test,
+    "org.scalatest"          %% "scalatest"      % "3.0.3"   % Test,
+    "org.scalacheck"         %% "scalacheck"     % "1.13.4"  % Test,
+    "org.pegdown"            % "pegdown"         % "1.6.0"   % Test,
+    "com.github.tomakehurst" % "wiremock"        % "1.52"    % Test,
+    "ch.qos.logback"         % "logback-classic" % "1.1.7"   % Test,
+    "org.mockito"            % "mockito-all"     % "1.10.19" % Test
   )
 
 }
