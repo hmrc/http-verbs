77b12b407c2d6e7a36ff857ad58358609a54d70b
jopintopaul <joanna.pinto@gmail.com>
2020-03-30 10:34:19 +0100

    BDOG-736: Moving to multi module project

--- build.sbt
+++ build.sbt
@@ -3,20 +3,72 @@ import sbt._
 
 val name = "http-verbs"
 
-lazy val library = Project(name, file("."))
+val scala2_11 = "2.11.12"
+val scala2_12 = "2.12.8"
+
+// Disable multiple project tests running at the same time: https://stackoverflow.com/questions/11899723/how-to-turn-off-parallel-execution-of-tests-for-multi-project-builds
+// TODO: restrict parallelExecution to tests only (the obvious way to do this using Test scope does not seem to work correctly)
+parallelExecution in Global := false
+
+lazy val commonSettings = Seq(
+  organization := "uk.gov.hmrc",
+  majorVersion := 10,
+  makePublicallyAvailableOnBintray := true,
+  resolvers := Seq(
+    Resolver.bintrayRepo("hmrc", "releases"),
+    Resolver.typesafeRepo("releases")
+  )
+)
+
+lazy val library = (project in file("."))
   .enablePlugins(SbtAutoBuildPlugin, SbtGitVersioning, SbtArtifactory)
   .settings(
-    majorVersion                     := 10,
-    makePublicallyAvailableOnBintray := true
-  ).settings(
-    scalaVersion        := "2.11.12",
-    crossScalaVersions  := Seq("2.11.12", "2.12.8"),
-    libraryDependencies ++= AppDependencies.compile ++ AppDependencies.test,
-    PlayCrossCompilation.playCrossCompilationSettings,
-    scalacOptions       ++= Seq("-deprecation"),
-    resolvers           := Seq(
-      Resolver.bintrayRepo("hmrc", "releases"),
-      Resolver.typesafeRepo("releases")
-    )
+    commonSettings,
+    publish := {},
+    publishAndDistribute := {},
+    crossScalaVersions := Seq.empty
+  )
+  .aggregate(
+    httpVerbsPlay25,
+    httpVerbsPlay26,
+    httpVerbsPlay27
   )
   .disablePlugins(sbt.plugins.JUnitXmlReportPlugin)
+
+lazy val httpVerbsCommon = Project("http-verbs-common", file("http-verbs-common"))
+  .disablePlugins(SbtGitVersioning)
+
+lazy val httpVerbsPlay25 = Project("http-verbs-play-25", file("http-verbs-play-25"))
+  .enablePlugins(SbtAutoBuildPlugin, SbtArtifactory)
+  .settings(
+    commonSettings,
+    unmanagedSourceDirectories in Compile += (httpVerbsCommon / Compile / scalaSource).value,
+    unmanagedSourceDirectories in Test += (httpVerbsCommon / Test / scalaSource).value,
+    crossScalaVersions := Seq(scala2_11),
+    libraryDependencies ++= AppDependencies.compileCommon ++ AppDependencies.compilePlay25 ++ AppDependencies.testCommon ++ AppDependencies.testPlay25,
+    Test / fork := true // akka is not unloaded properly, which can affect other tests
+  )
+
+lazy val httpVerbsPlay26 = Project("http-verbs-play-26", file("http-verbs-play-26"))
+  .enablePlugins(SbtAutoBuildPlugin, SbtArtifactory)
+  .settings(
+    commonSettings,
+    crossScalaVersions := Seq(scala2_11, scala2_12),
+    libraryDependencies ++= AppDependencies.compileCommon ++ AppDependencies.compilePlay26 ++ AppDependencies.testCommon ++ AppDependencies.testPlay26,
+    unmanagedSourceDirectories in Compile += (httpVerbsCommon / Compile / scalaSource).value,
+    unmanagedSourceDirectories in Test += (httpVerbsCommon / Test / scalaSource).value,
+    Test / fork := true // akka is not unloaded properly, which can affect other tests
+  )
+
+lazy val httpVerbsPlay27 = Project("http-verbs-play-27", file("http-verbs-play-27"))
+  .enablePlugins(SbtAutoBuildPlugin, SbtArtifactory)
+  .settings(
+    commonSettings,
+    crossScalaVersions := Seq(scala2_12),
+    libraryDependencies ++= AppDependencies.compileCommon ++ AppDependencies.compilePlay27 ++ AppDependencies.testCommon ++ AppDependencies.testPlay27,
+    unmanagedSourceDirectories in Compile += (httpVerbsCommon / Compile / scalaSource).value,
+    unmanagedSourceDirectories in Test += (httpVerbsCommon / Test / scalaSource).value,
+    scalaSource in Compile := (httpVerbsPlay26 / Compile / scalaSource).value,
+    scalaSource in Test := (httpVerbsPlay26 / Test / scalaSource).value,
+    Test / fork := true // akka is not unloaded properly, which can affect other tests
+  )
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,19 +1,3 @@
-/*
- * Copyright 2018 HM Revenue & Customs
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *     http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
-
 import sbt._
 
 object AppDependencies {
@@ -22,68 +6,65 @@ object AppDependencies {
   val play26Version = "2.6.20"
   val play27Version = "2.7.4"
 
-  val compile: Seq[ModuleID] =
-    PlayCrossCompilation.dependencies(
-      shared = Seq(
-        "com.typesafe" % "config"    % "1.3.3",
-        "org.slf4j"    % "slf4j-api" % "1.7.25",
-        // empty http-core added to force eviction 
-        // as classes from this lib have been inlined in http-verbs
-        "uk.gov.hmrc" %% "http-core"          % "2.2.0",
-        // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
-        "com.fasterxml.jackson.core"     % "jackson-core"            % "2.9.8",
-        "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.9.8",
-        "com.fasterxml.jackson.core"     % "jackson-annotations"     % "2.9.8",
-        "com.fasterxml.jackson.datatype" % "jackson-datatype-jdk8"   % "2.9.8",
-        "com.fasterxml.jackson.datatype" % "jackson-datatype-jsr310" % "2.9.8"
-      ),
-      play25 = Seq(
-        "com.typesafe.play" %% "play-json" % play25Version,
-        "com.typesafe.play" %% "play-ws"   % play25Version,
-        // force dependencies due to security flaws found in xercesImpl 2.11.0
-        // only applies to play 2.5 since it was removed from play 2.6
-        // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
-        "xerces" % "xercesImpl" % "2.12.0",
-        // empty http-verbs-play-25 added to force eviction
-        // as classes from this lib have been inlined in http-verbs
-        "uk.gov.hmrc" %% "http-verbs-play-25" % "1.0.0"
-      ),
-      play26 = Seq(
-        "com.typesafe.play" %% "play-json" % "2.6.9",
-        "com.typesafe.play" %% "play-ws"   % play26Version
-      ),
-      play27 = Seq(
-        "com.typesafe.play" %% "play-json" % play27Version,
-        "com.typesafe.play" %% "play-ws"   % play27Version
-      )
-    )
-
-  val test: Seq[ModuleID] = PlayCrossCompilation.dependencies(
-    shared = Seq(
-      "commons-codec"          % "commons-codec"   % "1.12"     % Test,
-      "org.scalatest"          %% "scalatest"      % "3.1.1"   % Test,
-      "org.scalatestplus"      %% "scalatestplus-mockito" % "1.0.0-M2" % Test,
-      "org.scalatestplus"      %% "scalatestplus-scalacheck"  % "3.1.0.0-RC2" % Test,
-      "org.scalacheck"         %% "scalacheck"     % "1.14.0"  % Test,
-      "com.github.tomakehurst" % "wiremock"        % "1.58"    % Test,
-      "ch.qos.logback"         % "logback-classic" % "1.2.3"   % Test,
-      "ch.qos.logback"         % "logback-core"    % "1.2.3"   % Test,
-      "org.mockito"            % "mockito-all"     % "1.10.19" % Test,
-      "org.webbitserver"       % "webbit"          % "0.4.15"  % Test,
-      "com.vladsch.flexmark"   % "flexmark-all"    % "0.35.10" % Test,
-    ),
-    play25 = Seq(
-      "com.typesafe.play" %% "play-test"   % play25Version % Test,
-      "com.typesafe.play" %% "play-specs2" % play25Version % Test
-    ),
-    play26 = Seq(
-      "com.typesafe.play" %% "play-test"   % play26Version % Test,
-      "com.typesafe.play" %% "play-ahc-ws" % play26Version % Test
-    ),
-    play27 = Seq(
-      "com.typesafe.play" %% "play-test"   % play27Version % Test,
-      "com.typesafe.play" %% "play-ahc-ws" % play27Version % Test
-    )
+  val compileCommon = Seq(
+    "com.typesafe" % "config"    % "1.3.3",
+    "org.slf4j"    % "slf4j-api" % "1.7.25",
+    // empty http-core added to force eviction
+    // as classes from this lib have been inlined in http-verbs
+    "uk.gov.hmrc" %% "http-core"          % "2.2.0",
+    // force dependencies due to security flaws found in jackson-databind < 2.9.x using XRay
+    "com.fasterxml.jackson.core"     % "jackson-core"            % "2.10.3",
+    "com.fasterxml.jackson.core"     % "jackson-databind"        % "2.10.3"
   )
 
+  val compilePlay25 = Seq(
+    "com.typesafe.play" %% "play-json" % play25Version,
+    "com.typesafe.play" %% "play-ws"   % play25Version,
+    // force dependencies due to security flaws found in xercesImpl 2.11.0
+    // only applies to play 2.5 since it was removed from play 2.6
+    // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
+    "xerces" % "xercesImpl" % "2.12.0",
+    // empty http-verbs-play-25 added to force eviction
+    // as classes from this lib have been inlined in http-verbs
+    "uk.gov.hmrc" %% "http-verbs-play-25" % "1.0.0"
+  )
+
+  val compilePlay26 = Seq(
+    "com.typesafe.play" %% "play-json" % "2.6.9",
+    "com.typesafe.play" %% "play-ws"   % play26Version
+  )
+
+  val compilePlay27 = Seq(
+    "com.typesafe.play" %% "play-json" % play27Version,
+    "com.typesafe.play" %% "play-ws"   % play27Version
+  )
+
+  val testCommon = Seq(
+    "commons-codec"          % "commons-codec"   % "1.12"    % Test,
+    "org.scalatest"          %% "scalatest"      % "3.1.1"   % Test,
+    "org.scalatestplus"      %% "scalatestplus-mockito" % "1.0.0-M2" % Test,
+    "org.scalatestplus"      %% "scalatestplus-scalacheck"  % "3.1.0.0-RC2" % Test,
+    "org.scalacheck"         %% "scalacheck"     % "1.14.0"  % Test,
+    "com.github.tomakehurst" % "wiremock"        % "1.58"    % Test,
+    "ch.qos.logback"         % "logback-classic" % "1.2.3"   % Test,
+    "ch.qos.logback"         % "logback-core"    % "1.2.3"   % Test,
+    "org.mockito"            % "mockito-all"     % "1.10.19" % Test,
+    "org.webbitserver"       % "webbit"          % "0.4.15"  % Test,
+    "com.vladsch.flexmark"   % "flexmark-all"    % "0.35.10" % Test
+  )
+
+  val testPlay25 = Seq(
+    "com.typesafe.play" %% "play-test"   % play25Version % Test,
+    "com.typesafe.play" %% "play-specs2" % play25Version % Test
+  )
+
+  val testPlay26 = Seq(
+    "com.typesafe.play" %% "play-test"   % play26Version % Test,
+    "com.typesafe.play" %% "play-ahc-ws" % play26Version % Test
+  )
+
+  val testPlay27 = Seq(
+    "com.typesafe.play" %% "play-test"   % play27Version % Test,
+    "com.typesafe.play" %% "play-ahc-ws" % play27Version % Test
+  )
 }
