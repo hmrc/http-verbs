f2dabcffc1e234d3f0a4bcff6b9362a0888326d1
colin-lamed <9568290+colin-lamed@users.noreply.github.com>
2020-04-02 15:24:00 +0100

    BDOG-736 Fix circular dependency for play-25 build

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,10 +23,7 @@ object AppDependencies {
     // force dependencies due to security flaws found in xercesImpl 2.11.0
     // only applies to play 2.5 since it was removed from play 2.6
     // https://github.com/playframework/playframework/blob/master/documentation/manual/releases/release26/migration26/Migration26.md#xercesimpl-removal
-    "xerces" % "xercesImpl" % "2.12.0",
-    // empty http-verbs-play-25 added to force eviction
-    // as classes from this lib have been inlined in http-verbs
-    "uk.gov.hmrc" %% "http-verbs-play-25" % "1.0.0"
+    "xerces" % "xercesImpl" % "2.12.0"
   )
 
   val compilePlay26 = Seq(
